b0aab70bd4b8b6941e9bfe5c360fbfb7a16ebd49
irotsunagu <admin@irotsunagu.com>
2024-02-19 14:50:52 +0900

    mongo access

--- src/main/java/com/example/sample/model/PinRecord.java
+++ src/main/java/com/example/sample/model/PinRecord.java
@@ -1,16 +1,20 @@
 package com.example.sample.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import jakarta.validation.constraints.NotNull;
 
 /**
  * ピンの情報を規定する型（Entity、record）
  */
+@Document
 public record PinRecord(
-                @NotNull(message = "id is required") String id,
-                @NotNull(message = "title is required") String title,
-                @NotNull(message = "description is required") String description,
-                @NotNull(message = "latitude is required") double latitude,
-                @NotNull(message = "longitude is required") double longitude,
-                @NotNull(message = "category is required") String category,
-                @NotNull(message = "imageUrl is required") String imageUrl) {
+        @Id @NotNull(message = "id is required") String id,
+        @NotNull(message = "title is required") String title,
+        @NotNull(message = "description is required") String description,
+        @NotNull(message = "latitude is required") double latitude,
+        @NotNull(message = "longitude is required") double longitude,
+        @NotNull(message = "category is required") String category,
+        @NotNull(message = "imageUrl is required") String imageUrl) {
 }
